64ac6d71014fc58d94b90bc152a2b302a104e464
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-12-23 15:58:20 +0000

    fixed

--- Multiplication.java
+++ Multiplication.java
@@ -5,11 +5,21 @@ public class Multiplication {
 	public static void main(String[] args) {
 		System.out.print("Enter first number: ");
 		Scanner sc = new Scanner(System.in);
-		double num1 = sc.nextDouble();
+		int num1 = sc.nextInt();
 		System.out.print("Enter second number: ");
 		sc = new Scanner(System.in);
-		double num2 = sc.nextDouble();
-		double result = num1 * num2;
+		int num2 = sc.nextInt();
+		int result = 0;
+		int count = 0;
+		if (num2 > 0) {
+			for(count = 1; count <= num2; count++) {
+				result = result + num1;
+			}
+		} else if (num2 < 0) {
+			for(count = -1; count >= num2; count--) {
+				result = result - num1;
+			}
+		}		
 		System.out.println(num1 + " multiplied by " + num2 + " equals " + result + ".");
 	}
 }
